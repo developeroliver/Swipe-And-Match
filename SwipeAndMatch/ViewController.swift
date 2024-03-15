4471efca8cd404b0f3a61ad2b68d256421deb5f7
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-15 20:18:15 +0100

    Create stack view for homeVC

--- SwipeAndMatch/ViewController.swift
+++ SwipeAndMatch/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        
+      
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { color -> UIView  in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        
+        let blueView = UIView()
+        let yellowView = UIView()
+        
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        blueView.backgroundColor = .systemBlue
+        yellowView.backgroundColor = .systemYellow
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.fillSuperview()
     }
-
-
 }
 
